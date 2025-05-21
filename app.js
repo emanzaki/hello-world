4764a25f955656225cb36d493830c0922bca2fbe
Eman Zaki <88564710+emanzaki@users.noreply.github.com>
2025-05-21 19:10:08 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,1 @@
-const express = require('express');
-const app = express();
-const PORT = 3000;
-
-app.get('/', (req, res) => {
-  res.send('Hello, Docker from Node.js!');
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+console.log('Hello World! from app.js')
